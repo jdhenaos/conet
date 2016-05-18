8fc6e74a75d74412538a6b52edf5e003ad9056d4
juanhenao <judhenaosa@unal.edu.co>
2016-05-18 16:52:43 -0500

    init

--- conet.R
+++ conet.R
@@ -5,6 +5,23 @@ library(vsn)
 
 alzGSM <- read.table("Descargas/Alzheimer_Chips.txt",stringsAsFactors = F)
 
+############# FUNCION ##############
+
+setwd("alzheimer/")
+
 for(i in alzGSM$V1){
   getGEOSuppFiles(i)
 }
+
+files <- dir(".")[grep("GSE",dir("."))]
+
+for(j in files){
+  untar(paste0(j,"/",j,"_RAW.tar"), exdir = paste0(j,"/"))
+}
+
+getGEOfile("GPL570", destdir = ".")
+
+####################################
+
+############# FUNCION ##############
+
