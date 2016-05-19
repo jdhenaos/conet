c93586faeb6e54a0c2c3205ea4fab561740d0c6a
juanhenao <judhenaosa@unal.edu.co>
2016-05-18 19:03:14 -0500

    init

--- conet.R
+++ conet.R
@@ -3,33 +3,41 @@ library(siggenes)
 library(GEOquery)
 library(vsn)
 
-alzGSM <- read.table("Descargas/Alzheimer_Chips.txt",stringsAsFactors = F)
+alzGSM <- read.table("Alzheimer_Chips.txt",stringsAsFactors = F)
 
 ############# FUNCION ##############
 
-setwd("alzheimer/")
-
-for(i in alzGSM$V1){
-  getGEOSuppFiles(i)
+GetInfo <- function(GSE,GPL,dir="."){
+  
+  setwd(dir)
+  
+  for(i in GSE){
+    getGEOSuppFiles(i)
+  }
+  
+  files <- dir(".")[grep("GSE",dir("."))]
+  
+  for(j in files){
+    untar(paste0(j,"/",j,"_RAW.tar"), exdir = paste0(j,"/"))
+  }
+  
+  getGEOfile(GPL, destdir = ".")
 }
-
-files <- dir(".")[grep("GSE",dir("."))]
-
-for(j in files){
-  untar(paste0(j,"/",j,"_RAW.tar"), exdir = paste0(j,"/"))
-}
-
-getGEOfile("GPL570", destdir = ".")
 
 ####################################
 
 ############# FUNCION ##############
 
-raw <- read.table(file = paste0("GSE16759/","filelist.txt"),sep = "\t",
-                  header = T,comment.char = "#",stringsAsFactors = F)
-GSMs <- raw[,2]
-GSMs <- GSMs[grep(".CEL",GSMs)]
-affy <- ReadAffy(filenames = as.character(GSMs), compress = T,celfile.path = "GSE16759")
+getaffy <- function(GSE){
+  raw <- read.table(file = paste0(GSE,"/","filelist.txt"),sep = "\t",
+                    header = T,comment.char = "#",stringsAsFactors = F)
+  GSMs <- raw[,2]
+  GSMs <- GSMs[grep(".CEL",GSMs)]
+  affy <- ReadAffy(filenames = as.character(GSMs), compress = T,
+                   celfile.path = GSE)
+  
+}
+
 
 ###################################
 
@@ -37,11 +45,14 @@ gene <- GeneSymbol("GPL570")
 
 ########## FUNCION ################
 
-rma <- rma(affy)
-eset <- ProbeFilter(rma,gene)
-y=c(rep(1,4),rep(0,4))
-matrix <- as.matrix(eset)
-sam <- sam(matrix,y)
+difexprs <- function(affy,treatment){
+  rma <- rma(affy)
+  eset <- ProbeFilter(rma,gene)
+  y=treatment
+  matrix <- as.matrix(eset)
+  sam <- sam(matrix,y)
+}
+
 
 #################################
 
