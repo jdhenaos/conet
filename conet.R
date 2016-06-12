035beed57d556d4952127efcdbc491dea0129a2c
juanhenao <judhenaosa@unal.edu.co>
2016-06-11 22:32:46 -0500

    more

--- conet.R
+++ conet.R
@@ -222,6 +222,6 @@ write.table(row.names(Adife),file = "GSE66333GeneList.txt",quote = F)
 Anet <- CreateNet(difexp = Adife)
 write.graph(Anet,file = "GSE66333.txt",format = "ncol")
 
-new <- stp(eset,t,th = 0.2)
+new <- stp(eset,t,R = 100, PER = T)
 plot(new)
                                                                                                                                           
