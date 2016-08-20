d42dca8d899c95fcbc3a91803d04276c171f6611
juancholkovich <juan@tesla.juan.henao>
2016-08-20 13:45:26 -0500

    package

--- gene.symbol.R
+++ gene.symbol.R
@@ -3,12 +3,20 @@
 # Bioinformatics and Systems Biology | Universidad Nacional de Colombia
 
 gene.symbol <- function(GPL, d = "."){
+  
   # Extrae la informacion del archivo .soft
+  
   gpl <- getGEO(filename = paste0(GPL,".soft"))
+  
   # Crea una tabla con todos los datos en el GPL
+  
   sym <- Table(gpl)
+  
   # Crea un data.frame con las sondas asociadas al gen al que corresponden
+  
   ta <- data.frame(sym$ID, gsub(" /// ","-",sym$`Gene Symbol`), stringsAsFactors = F)
+  
   names(ta) <- c("probe","gene")
+  
   return(ta)
 }
