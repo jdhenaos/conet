49bff29c14ddf35792dc9dee5c8da87f71cbda80
juan henao <judhenaosa@unal.edu.co>
2016-08-20 15:56:14 -0500

    package

--- dif.exprs.R
+++ dif.exprs.R
@@ -52,18 +52,44 @@ dif.exprs <- function(affy,genes,treatment,fdr,NormalizeMethod,SummaryMethod,Dif
   
   if(DifferentialMethod == "sam"){
     
-    # 
+    # Differential analysis using am method
     
     samr <- sam(data = eset,cl = treatment,B=100,rand=100)
+    
+    # Obtain the fdr to different tresholds
+    
     tab <- as.data.frame(samr@mat.fdr)
+    
+    # Filter the fdr values from the expected value
+    
     tab <- tab[tab$FDR >= fdr,]
+    
+    # Menssage to empty result
+    
     if(nrow(tab) == 0){stop("No differentially expressed genes found")}
+    
+    # Obtain the threshold value
+    
     value <- tab[nrow(tab),]
+    
+    # Plotting the result of differential analisys
+    
     plot(samr,value$Delta)
+    
+    # Summarizing the results of differential analisys
+    
     sum <- summary(samr,value$Delta,entrez=F)
+    
+    # Obtain the names of genes differentially expressed
+    
     dife <- sum@row.sig.genes
+    
+    # Filter the expression matrix with the genes diferentially expressed
+    
     genes <- eset[dife,]
+    
     print(paste0("Achieved FDR: ",value$FDR))
+    
   }else if(DifferentialMethod == "acde"){
     treatment[treatment == 0] <- 2
     acde <- stp(eset,treatment,R = 100, PER = T,alpha = fdr)
